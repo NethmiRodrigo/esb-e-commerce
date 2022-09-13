b046e5c3078798b961814f1c2484709ee5310e3a
NethmiRodrigo <neth23rodrigo@gmail.com>
2022-09-13 21:51:48 +0530

    Removed console log

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -14,7 +14,6 @@ export default function EcommerceShop() {
   const fetchProducts = async () => {
     try {
       const result = await axios.get('http://localhost:5000/products');
-      console.log(result.data);
       setProducts(result.data);
     } catch (error) {
       console.error(error);
