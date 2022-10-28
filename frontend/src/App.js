186fcc1382f08ed3b55570bd87bfd02dc558ccd8
NethmiRodrigo <neth23rodrigo@gmail.com>
2022-10-28 15:07:45 +0530

    Added interceptor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 // routes
 import Router from './routes';
 // theme
 import ThemeProvider from './theme';
 // components
 import ScrollToTop from './components/ScrollToTop';
+import { JWT_TOKEN } from './utils/constants';
 
 // ----------------------------------------------------------------------
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('Token');
+    if (token) {
+      const decodedToken = jwtDecode(token, JWT_TOKEN);
+      if (decodedToken.exp * 1000 < Date.now()) {
+        localStorage.removeItem('Token');
+      } else config.headers.authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
 
 export default function App() {
   return (
